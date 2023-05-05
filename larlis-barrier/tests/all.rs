256f1edae0a8fa1c31f645b39ccc3a8a16640c5d
sgdxbc <sung@comp.nus.edu.sg>
2023-05-05 06:44:37 +0000

    Add TCP based barrier test

--- larlis-barrier/tests/all.rs
+++ larlis-barrier/tests/all.rs
@@ -47,7 +47,7 @@ async fn provide_barrier_udp(addr: SocketAddr, count: usize) {
 }
 
 #[tokio::test]
-async fn sync_two() {
+async fn sync_udp() {
     const N: usize = 2;
 
     let service_addr = SocketAddr::from(([0, 0, 0, 0], 60000));
@@ -73,6 +73,28 @@ async fn sync_two() {
     service.await.unwrap()
 }
 
+async fn use_barrier_tcp(addr: SocketAddr, service: SocketAddr) -> Message<UserPayload> {
+    let mut message = Drive::<Message<UserPayload>>::default();
+    let mut connection = larlis_tcp::Connection::connect(
+        addr,
+        service,
+        de().install(Closure::from(|(_, message)| message).install(message.state())),
+        Drive::<()>::default().state(),
+    )
+    .await;
+    let local_message = connection.stream.local_addr().unwrap().port();
+    let mut dispatch = Dispatch::default();
+    dispatch.insert_state(connection.remote_addr, connection.egress_state());
+    let connection = spawn(async move { connection.start().await });
+
+    ser().install(dispatch).update((service, local_message));
+
+    let mut message = message.recv().await.unwrap();
+    connection.abort();
+    message.sort();
+    message
+}
+
 async fn provide_barrier_tcp(addr: SocketAddr, count: usize) {
     let app_drive = Drive::<(SocketAddr, UserPayload)>::default();
     let mut finished = Drive::<()>::default();
@@ -81,17 +103,45 @@ async fn provide_barrier_tcp(addr: SocketAddr, count: usize) {
     let listener = larlis_tcp::Listener::bind(addr);
     let mut dispatch = Dispatch::default();
     for _ in 0..count {
-        let connection = listener
+        let mut connection = listener
             .accept(
                 de::<UserPayload>().install(app_drive.state()),
                 disconnected_drive.state(),
             )
             .await;
         dispatch.insert_state(connection.remote_addr, connection.egress_state());
+        spawn(async move { connection.start().await });
     }
     let app = Service::new(ser().install(dispatch), finished.state(), count);
 
     let app = spawn(async move { app_drive.run(app).await });
     finished.recv().await.unwrap();
     app.await.unwrap() //
+}
+
+#[tokio::test]
+async fn sync_tcp() {
+    const N: usize = 2;
+
+    let service_addr = SocketAddr::from(([0, 0, 0, 0], 60000));
+    let service = spawn(provide_barrier_tcp(service_addr, N));
+    // wait for service up?
+
+    let tasks = Vec::from_iter(
+        repeat_with(|| {
+            spawn(use_barrier_tcp(
+                SocketAddr::from(([0, 0, 0, 0], 0)),
+                service_addr,
+            ))
+        })
+        .take(N),
+    );
+    let mut messages = Vec::new();
+    for task in tasks {
+        messages.push(task.await.unwrap());
+    }
+    println!("{messages:?}");
+    assert_eq!(messages[0], messages[1]);
+
+    service.await.unwrap()
 }
--- larlis-tcp/src/lib.rs
+++ larlis-tcp/src/lib.rs
@@ -1,167 +1,12 @@
-use std::{collections::HashMap, net::SocketAddr};
+use std::net::SocketAddr;
 
-use larlis_core::actor::{SharedClone, State};
+use larlis_core::actor::State;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, BufStream},
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpSocket, TcpStream},
-    select, spawn,
+    select,
     sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
 };
-
-pub type Message = (SocketAddr, Vec<u8>);
-
-#[derive(Debug)]
-pub enum TransportMessage {
-    Accept(SocketAddr, TcpStream),
-    Send(SocketAddr, Vec<u8>),
-    Close(SocketAddr),
-}
-
-impl From<(SocketAddr, Vec<u8>)> for TransportMessage {
-    fn from(value: (SocketAddr, Vec<u8>)) -> Self {
-        let (dest, buf) = value;
-        Self::Send(dest, buf)
-    }
-}
-
-// if behave as a client, i.e. never `accept` any connection, this `Transport`
-// may be embedded into upstream state
-// so upstream probably need to be wrapped into `Drive` to work with `Transport`
-// i.e. `S` should be a `DriveState`, to avoid circular ownership (and also
-// fulfill the `SharedClone` bound)
-pub struct Transport<S> {
-    addr: SocketAddr,
-    egress: HashMap<SocketAddr, UnboundedSender<Vec<u8>>>,
-    pub state: S,
-}
-
-impl<S> Transport<S> {
-    pub fn bind(addr: SocketAddr, state: S) -> Self {
-        Self {
-            addr,
-            egress: Default::default(),
-            state,
-        }
-    }
-
-    async fn run_connnection(
-        remote: SocketAddr,
-        stream: TcpStream,
-        mut state: S,
-        mut egress: UnboundedReceiver<Vec<u8>>,
-    ) where
-        S: for<'m> State<'m, Message = (SocketAddr, &'m [u8])> + Send + 'static,
-    {
-        assert_eq!(stream.peer_addr().unwrap(), remote);
-        // i don't think we need concurrent read-write support
-        // we should use this transport to support some stop-go protocols
-        // and write some dedicated implementation for massive bidirectional
-        // data exchange
-
-        let mut stream = BufStream::new(stream);
-        let mut buf = vec![0; 65536]; //
-        loop {
-            select! {
-                result = stream.read_u32() => {
-                    let len = result.unwrap(); // TODO
-                    stream.read_exact(&mut buf[..len as _]).await.unwrap();
-                    state.update((remote, &buf[..len as _]));
-                }
-                message = egress.recv() => {
-                    let message = message.unwrap(); // TODO
-                    assert!(message.len() <= u32::MAX as _);
-                    stream.write_u32(message.len() as _).await.unwrap();
-                    stream.write_all(&message).await.unwrap();
-                }
-            }
-        }
-    }
-}
-
-impl<S> State<'_> for Transport<S>
-where
-    S: SharedClone + for<'m> State<'m, Message = (SocketAddr, &'m [u8])> + Send + 'static,
-{
-    type Message = TransportMessage;
-
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            // `connect` 5-tuple, then `accept` the same 5-tuple almost at the
-            // same time, i.e. both side `connect` to each other
-            // assuming `stream` will be exact the same instance of the one
-            // already in `run_connection` (and channeled in `egress`)
-            // hope that is true (or even better this case never happen)
-            TransportMessage::Accept(remote, stream) => {
-                self.egress.entry(remote).or_insert_with(|| {
-                    let egress = unbounded_channel();
-                    spawn(Self::run_connnection(
-                        remote,
-                        stream,
-                        self.state.clone(),
-                        egress.1,
-                    ));
-                    egress.0
-                });
-            }
-
-            TransportMessage::Send(dest, buf) => {
-                let state = self.state.clone();
-                let addr = self.addr;
-                self.egress
-                    .entry(dest)
-                    .or_insert_with(move || {
-                        let egress = unbounded_channel();
-                        spawn(async move {
-                            let socket = TcpSocket::new_v4().unwrap();
-                            socket.set_reuseaddr(true).unwrap(); //
-                            socket.bind(addr).unwrap();
-                            Self::run_connnection(
-                                dest,
-                                socket.connect(dest).await.unwrap(),
-                                state,
-                                egress.1,
-                            )
-                            .await;
-                        });
-                        egress.0
-                    })
-                    .send(buf)
-                    .unwrap();
-            }
-
-            TransportMessage::Close(remote) => {
-                self.egress.remove(&remote); // assert exist and closed?
-            }
-        }
-    }
-}
-
-pub struct Accept<S> {
-    listener: TcpListener,
-    pub state: S,
-}
-
-impl<S> Accept<S> {
-    pub fn bind(addr: SocketAddr, state: S) -> Self {
-        let socket = TcpSocket::new_v4().unwrap();
-        socket.set_reuseaddr(true).unwrap(); //
-        socket.bind(addr).unwrap();
-        Self {
-            listener: socket.listen(4096).unwrap(),
-            state,
-        }
-    }
-
-    pub async fn start(&mut self)
-    where
-        S: for<'m> State<'m, Message = TransportMessage>,
-    {
-        loop {
-            let (stream, remote) = self.listener.accept().await.unwrap();
-            self.state.update(TransportMessage::Accept(remote, stream));
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct Connection<S, D> {
@@ -190,7 +35,7 @@ impl<S, D> Connection<S, D> {
         local_addr: SocketAddr,
         remote_addr: SocketAddr,
         state: S,
-        finished: D,
+        disconnected: D,
     ) -> Self {
         let socket = TcpSocket::new_v4().unwrap();
         socket.set_reuseaddr(true).unwrap();
@@ -199,7 +44,7 @@ impl<S, D> Connection<S, D> {
             socket.connect(remote_addr).await.unwrap(),
             remote_addr,
             state,
-            finished,
+            disconnected,
         )
     }
 
