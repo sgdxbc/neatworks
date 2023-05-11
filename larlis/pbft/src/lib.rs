f271fa04d06600102008c48143990f780e76141b
sgdxbc <sung@comp.nus.edu.sg>
2023-05-11 12:46:29 +0000

    Add PBFT client

---
+++ larlis/pbft/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod client;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Request {
+    client_id: u32,
+    request_num: u32,
+    op: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Reply {
+    request_num: u32,
+    result: Vec<u8>,
+    replica_id: u8,
+}
