05c7443e2026ffcc36ff4984de57289ac403e4cd
sgdxbc <sung@comp.nus.edu.sg>
2023-10-18 04:39:12 +0000

    Add kademlia messages

--- crates/kademlia/src/lib.rs
+++ crates/kademlia/src/lib.rs
@@ -1,1 +1,43 @@
+use serde::{Deserialize, Serialize};
 
+pub type PeerId = [u8; 32];
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum Message {
+    // p2p messages
+    Find(Find),
+    FindOk(FindOk),
+    // rpc messages
+    Query(Query),
+    CancelQuery(CancelQuery),
+    QueryStatus(QueryStatus),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Find {
+    target: PeerId,
+    peer_id: PeerId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct FindOk {
+    target: PeerId,
+    //
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Query {
+    query_num: u32,
+    target: PeerId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CancelQuery {
+    query_num: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct QueryStatus {
+    query_num: u32,
+    //
+}
