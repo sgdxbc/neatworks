324620eb0966a217882d3934aa93360a69e969e6
sgdxbc <sung@comp.nus.edu.sg>
2023-12-04 08:28:56 +0000

    Add entropy messages

---
+++ crates/entropy/src/lib.rs
@@ -0,0 +1,42 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use halloween::{
+    crypto::Packet,
+    kademlia::{self, Location, PeerId},
+    transport::Addr,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub enum Message {
+    Kademlia(kademlia::Message),
+    SendFragment(SendFragment),
+    Publish(Packet),
+    PublishOk(Packet),
+    Query(Packet),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+struct Publish {
+    chunk: Location,
+    id: PeerId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+struct PublishOk {
+    chunk: Location,
+    proof: (),
+    id: PeerId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub struct SendFragment {
+    chunk: Location,
+    index: u32,
+    // this should always be a SocketAddr, just for reusing borsh impl
+    listener: Addr,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+struct Query {
+    chunk: Location,
+    id: PeerId,
+}
